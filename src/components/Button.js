6550046bbe9ff5743dcafe1d3b37597e514ebcd0
bhartibhatia <bhartibhatia1997@gmail.com>
2020-10-09 23:16:49 +0530

    Navbar Part1 Completed

---
+++ src/components/Button.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import './Button.css'
+
+const STYLES = [
+    'btn-primary', 
+    'btn-outline'
+]
+
+const SIZES=[
+'btn-medium',
+'btn-large'
+]
+ export const Button = ({children,type,onClick,buttonStyle,buttonSize}) =>{}
